e396c615ba340cd147eb49908385e1cf9e130910
jterry94 <terryj@iit.edu>
2023-12-28 18:10:07 -0600

    Initial Commit

---
+++ Temperature Converter Observation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Temperature Converter Observation
+//
+//  Created by Jeff_Terry on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Temperature Converter Observation/Temperature_Converter_ObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_Converter_ObservationApp.swift
+//  Temperature Converter Observation
+//
+//  Created by Jeff_Terry on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_Converter_ObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
