c54a2f0f26f3cc3115d8b929b49d5693ad70d51c
jterry94 <terryj@iit.edu>
2023-12-28 20:01:33 -0600

    Updated Commit

--- Temperature Converter Observation/ContentView.swift
+++ Temperature Converter Observation/ContentView.swift
@@ -6,17 +6,105 @@
 //
 
 import SwiftUI
+import Observation
 
 struct ContentView: View {
+    @Bindable var  myTemperatureConversions = TemperatureConversions()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        HStack{
+            Text("Fahrenheit: ")
+            
+            TextField("Enter temperature in °F ...", text: $myTemperatureConversions.fahrenheitString, onEditingChanged: { (changed) in
+                print("onEditingChanged - \(changed)")
+            }){
+                print("Username onCommit")
+                
+                self.convertToC()
+                
+                
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .frame(width: 220, height: nil)
         }
-        .padding()
+        
+        HStack{
+            Text("Celsius: ")
+            
+            TextField("Enter temperature in °C ...", text: $myTemperatureConversions.celsiusString, onEditingChanged: { (changed) in
+                print("onEditingChanged - \(changed)")
+            }){
+                print("Username onCommit")
+                
+                self.convertToF()
+                
+                
+                
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .frame(width: 220, height: nil)
+            
+        }
+        
+        HStack{
+            Text("Conversion Direction: ")
+            
+            Text(myTemperatureConversions.direction )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 220, height: nil)
+        }
+        
     }
+    
+    func convertToF() {
+        
+        if let tempDouble = Double(myTemperatureConversions.celsiusString) {
+            
+            let cToFReturn = myTemperatureConversions.convertCToF(Temp: tempDouble)
+            
+            if cToFReturn.Completed {
+                
+                myTemperatureConversions.celsiusString =  String(format: "%.02f °C", myTemperatureConversions.temperatureCelsius)
+                
+                myTemperatureConversions.fahrenheitString =  String(format: "%.02f °F", cToFReturn.Value    )
+                
+            }
+            else{
+                
+                //Handle Error
+            }
+            
+            
+        }
+    }
+        
+    
+    func convertToC() {
+        
+        if let tempDouble = Double(myTemperatureConversions.fahrenheitString) {
+            
+            let fToCReturn = myTemperatureConversions.convertFToC(Temp: tempDouble)
+            
+            if fToCReturn.Completed {
+                
+                myTemperatureConversions.fahrenheitString =  String(format: "%.02f °F", myTemperatureConversions.temperatureFahrenheit)
+                
+                myTemperatureConversions.celsiusString =  String(format: "%.02f °C", fToCReturn.Value    )
+                
+            }
+            else{
+                
+                //Handle Error
+            }
+            
+            
+        }
+        
+    }
+    
+    
 }
 
 #Preview {
--- Temperature Converter Observation/TemperatureConversions.swift
+++ Temperature Converter Observation/TemperatureConversions.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import Observation
+
+@Observable class TemperatureConversions {
+    
+    var temperatureCelsius :Double = 0.0
+    var temperatureFahrenheit :Double = 32.0
+    var direction :String = "None"
+    var celsiusString = ""
+    var fahrenheitString = ""
+    
+    func convertFToC(Temp: Double) -> (Completed: Bool, Value: Double) {
+        
+        var tempCelsius = 0.0
+        
+        tempCelsius = 5.0/9.0 * (Temp - 32.0)
+        
+        temperatureCelsius = tempCelsius
+        temperatureFahrenheit = Temp
+        direction = "F To C"
+        
+        return(Completed: true, Value: tempCelsius)
+        
+    }
+    
+    func convertCToF(Temp: Double) -> (Completed: Bool, Value: Double) {
+        
+        var tempFahrenheit = 0.0
+        
+        tempFahrenheit = 9.0/5.0 * (Temp)  + 32.0
+        
+        temperatureCelsius = Temp
+        temperatureFahrenheit = tempFahrenheit
+        direction = "C To F"
+        
+        return(Completed: true, Value: tempFahrenheit)
+        
+    }
+    
+}
